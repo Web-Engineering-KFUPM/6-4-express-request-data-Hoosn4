cab9b3add8a08d291e2750660cf27d519c096132
ICS 104 Student <ics104@ccm.kfupm.edu.sa>
2026-04-08 08:38:36 +0300

    todoo4: have done userId param middleware validation and normalization

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -129,6 +129,16 @@ app.get("/echo", (req, res) => {
 
 
 // Route param middleware example: /users/42
+app.param("userId", (req, res, next, userId) => {
+   const userIdNum = Number(userId);
+
+   if (!Number.isFinite(userIdNum) || userIdNum <= 0) {
+      return res.status(400).json({ ok: false, error: "userId must be positive number" });
+   }
+
+   req.userIdNum = userIdNum;
+   return next();
+});
 
 
 // Route params: /users/:userId route
